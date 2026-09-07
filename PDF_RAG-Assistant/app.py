d5a4f9a8df64141bd20f968ebc26c06499861ce4
Maira <1912maira@gmail.com>
2026-09-07 12:05:35 -0700

    Update app.py

--- PDF_RAG-Assistant/app.py
+++ PDF_RAG-Assistant/app.py
@@ -13,7 +13,7 @@ from sentence_transformers import SentenceTransformer
 # Configuration
 # -----------------------------
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-GROQ_MODEL = "llama-3.3-70b-versatile"
+GROQ_MODEL = "openai/gpt-oss-120b"
 CHUNK_SIZE = 400
 CHUNK_OVERLAP = 80
 TOP_K = 5
